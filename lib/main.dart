b41d4e2a5d6cf51cba8bf6c8f3aadeb143782a44
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-01-29 23:51:27 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:marpp/screens/intro_screen.dart';
+import 'package:marpp/screens/register_screen.dart';
+import 'package:marpp/screens/login_screen.dart';
+import 'package:marpp/screens/splash_screen.dart';
+// import 'screens/pageView_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+        title: 'MARPP',
+        theme: ThemeData(
+          //primarySwatch: Color(0xff8C25F4)
+        ),
+        //home: Splash(),
+        initialRoute: Splash.id,
+        routes: {
+          Splash.id: (context) => Splash(),
+          IntroScreen.id: (context) => IntroScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+        }
+        //navigatorKey: StackedService.navigatorKey,
+        );
+  }
+}
